a2486c69c0f0d596bf973b74e9692b28abb8c706
deepakausingh <deepakausingh@gmail.com>
2025-11-29 14:25:44 +0530

    change

--- backend2/migration/001_create_stories.sql
+++
@@ -1,8 +0,0 @@
--- run this once (or let the server attempt to run it on startup)
-CREATE TABLE IF NOT EXISTS stories (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  content TEXT NOT NULL,
-  cover_image TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
