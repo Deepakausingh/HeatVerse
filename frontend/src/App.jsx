4357c21b715aa836872043ced67f12baf75a4c7a
deepakausingh <deepakausingh@gmail.com>
2025-11-29 15:21:25 +0530

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex-1 container">
+      <main className="flex-1">
         <Outlet />
       </main>
     </div>
